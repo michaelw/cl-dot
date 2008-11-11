d49633be03fbbda876d4ecb96aef7fffb8e593d4
Michael Weber <michaelw@foldr.org>
2008-11-11 14:43:29 +0000

    Added missing export for *neato-path*
    git-svn-id: http://svn.foldr.org/~michaelw/cl-dot/trunk@52 3eedd062-5d29-411c-93cb-ccde70ca5252

--- package.lisp
+++ package.lisp
@@ -4,6 +4,7 @@
   (:use :common-lisp)
   (:export #:attributed #:node
            #:*dot-path*
+           #:*neato-path*
            #:graph-object-knows-of #:graph-object-node
            #:graph-object-points-to #:graph-object-pointed-to-by
            #:generate-graph-from-roots
