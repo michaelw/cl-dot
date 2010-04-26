59611758db17c2b900280b6ba6c8937307359b7f
Michael Weber <michaelw@foldr.org>
2010-04-26 06:46:19 +0000

    Put deprecated symbols into extra export
    git-svn-id: http://svn.foldr.org/~michaelw/cl-dot/trunk@58 3eedd062-5d29-411c-93cb-ccde70ca5252

--- package.lisp
+++ package.lisp
@@ -7,11 +7,11 @@
            #:*neato-path*
            #:graph-object-knows-of #:graph-object-node
            #:graph-object-points-to #:graph-object-pointed-to-by
+           #:graph-object-edges
            #:generate-graph-from-roots
-           #:print-graph #:dot-graph
-
-           ;; deprecated
-           #:object-knows-of #:object-node
+           #:print-graph #:dot-graph)
+  ;; deprecated
+  (:export #:object-knows-of #:object-node
            #:object-points-to #:object-pointed-to-by
            #:generate-graph))
 
