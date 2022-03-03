1521fc3ca92cf8f7ff4b7a4ce8deba655c82afac
Eric Timmons <etimmons@mit.edu>
2022-03-02 22:15:26 -0500

    Export more symbols

--- package.lisp
+++ package.lisp
@@ -9,7 +9,10 @@
            #:graph-object-points-to #:graph-object-pointed-to-by
            #:graph-object-edges
            #:generate-graph-from-roots
-           #:print-graph #:dot-graph)
+           #:print-graph #:dot-graph
+           #:edge #:graph
+           #:id-of #:nodes-of #:edges-of #:attributes-of
+           #:source-of #:target-of)
   ;; deprecated
   (:export #:object-knows-of #:object-node
            #:object-points-to #:object-pointed-to-by
