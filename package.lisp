8651cbde41c8e378a223a1cd2db4a29b7f6eeb3a
Marco Heisig <marco.heisig@fau.de>
2022-04-21 10:23:01 +0200

    Export more symbols and bump version number.
    The additional exported symbols allow users to inspect and modify the
    internal graph data structure of cl-dot for their own purposes.
    
    Since these additional exports may break modules :USEing cl-dot, the least
    we can do is to also bump the version number.  But honestly, please stop
    :USEing other people's libraries and use :IMPORT-FROM or package local
    nicknames instead :)

--- package.lisp
+++ package.lisp
@@ -2,21 +2,45 @@
 
 (defpackage cl-dot
   (:use :common-lisp)
-  (:export #:attributed #:node #:cluster
-           #:*dot-path*
-           #:*neato-path*
-           #:graph-object-knows-of
-           #:graph-object-node
-           #:graph-object-cluster
-           #:graph-object-points-to
-           #:graph-object-pointed-to-by
-           #:graph-object-edges
-           #:generate-graph-from-roots
-           #:print-graph #:dot-graph)
-  ;; deprecated
-  (:export #:object-knows-of #:object-node
-           #:object-points-to #:object-pointed-to-by
-           #:generate-graph))
+  (:export
+   ;; Variables
+   #:*dot-path*
+   #:*neato-path*
+
+   ;; Classes
+   #:attributed
+   #:node
+   #:cluster
+   #:edge
+   #:graph
+
+   ;; Accessors
+   #:id-of
+   #:attributes-of
+   #:nodes-of
+   #:edges-of
+   #:source-of
+   #:target-of
+
+   ;; Graph Protocol
+   #:graph-object-knows-of
+   #:graph-object-node
+   #:graph-object-cluster
+   #:graph-object-points-to
+   #:graph-object-pointed-to-by
+   #:graph-object-edges
+   #:generate-graph-from-roots
+
+   ;; Miscellaneous
+   #:print-graph
+   #:dot-graph
+
+   ;; Deprecated
+   #:object-knows-of
+   #:object-node
+   #:object-points-to
+   #:object-pointed-to-by
+   #:generate-graph))
 
 
 
